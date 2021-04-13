Add correct KL loss to QED score predictor

linking/layers/gcn_encoders.py
@@ -64,7 +64,7 @@ class VariationalGATEncoder(torch.nn.Module):
         x = self.conv1(x, edge_index).relu()
         for i in range(self.hidden_layers):
             x = self.conv2(x, edge_index).relu()
-        return torch.sum(self.conv_mu(x, edge_index).softmax(dim=1), dim=0), torch.sum(self.conv_log_var(x, edge_index).softmax(dim=1), dim=0)
+        return torch.sum(self.conv_mu(x, edge_index), dim=0), torch.sum(self.conv_log_var(x, edge_index), dim=0)
 
 class GCNEncoder(torch.nn.Module):
     def __init__(self, in_channels: int, out_channels: int):

linking/layers/linear_encoders.py
@@ -113,7 +113,7 @@ class MLP(torch.nn.Module):
         self.hidden_layers = hidden_layers
 
     def forward(self, x):
-        x = self.l1(x).relu().softmax(dim=0)
+        x = self.l1(x).relu()
         for i in range(self.hidden_layers):
             x = self.l2(x).relu()
         return self.l3(x)

linking/model/build_model.py
@@ -64,11 +64,10 @@ def build_qed_model(config: Config, device) -> nn.Module:
     #    in_channels=config.ligand_encoder_in_channels, out_channels=config.ligand_encoder_out_channels, edge_dim=config.num_allowable_bonds)
     ligand_encoder = VariationalGATEncoder(in_channels=config.ligand_encoder_in_channels, out_channels=config.ligand_encoder_out_channels, hidden_layers=6)
     mlp = MLP(in_channels=config.ligand_encoder_out_channels, out_channels=1, hidden_layers=0)
-    sch = Sch(config.sch_net_hidden_channels)
+    # sch = Sch(config.sch_net_hidden_channels)
 
     model = QED(
         ligand_encoder,
-        sch,
         mlp,
         config, device)
     return model

linking/model/model_qed.py
@@ -3,11 +3,12 @@ from linking.config.config import Config
 from linking.layers.gcn_encoders import VariationalGATEncoder
 from linking.layers.geom_encoders import Sch
 from linking.layers.linear_encoders import MLP
+from torch.distributions import Normal
+import torch.nn.functional as F
 
 class QED(torch.nn.Module):
     def __init__(self,
                  ligand_encoder: VariationalGATEncoder,
-                 sch: Sch,
                  mlp: MLP,
                  config: Config,
                  device):
@@ -17,7 +18,6 @@ class QED(torch.nn.Module):
         self.ligand_encoder = ligand_encoder
 
         # coords
-        self.sch = sch  # encoding
 
         # prediction
         self.mlp = mlp
@@ -27,30 +27,21 @@ class QED(torch.nn.Module):
 
     def forward(self, data_pocket, data_ligand):
         # Initialise data variables -------------------------
-        # x_p, edge_index_p, edge_wight_p = data_pocket.x, data_pocket.edge_index, data_pocket.edge_attr
         x_l, edge_index_l, edge_attr_l, bfs_index, bfs_attr = data_ligand.x, data_ligand.edge_index, data_ligand.edge_attr, data_ligand.bfs_index.clone(), data_ligand.bfs_attr.clone()
-        # x_pos_p = data_pocket.x[:, 1:4].copy()
-        x_pos_l = data_ligand.x[:, 1:4]
-
-        # Encode -------------------------
-        # pocket gnn encoding
-        # z_pocket_atoms = self.pocket_encoder(x_p, edge_index_p)
-        # z_pocket = torch.mean(z_pocket_atoms, dim=0)
 
         # ligand gnn encoding
         z_mu, z_log_var = self.ligand_encoder(x_l, edge_index_l)
+        q = Normal(z_mu, 0.1 + F.softplus(z_log_var))
 
-        std = torch.exp(0.5 * z_log_var)
-        eps = torch.randn_like(std)
-        z_ligand = z_mu + std*eps
+        preds = []
+        for _ in range(20):
+            z_sample = q.rsample()
+            y = self.mlp(z_sample)
+            preds.append(y)
 
-        # ligand schnet encoding
-        # C = self.sch(torch.zeros(x_pos_l.size(0), device=self.device, dtype=torch.long), x_pos_l)
-        # C_avg = torch.mean(C, dim=0)
+        pred = torch.mean(torch.stack(preds, 1), 1)
 
-        y = self.mlp(z_ligand)
-
-        return y.squeeze(0), z_mu, z_log_var
+        return pred, z_mu, z_log_var, q
 
     def parameters(self):
        return [
@@ -61,5 +52,4 @@ class QED(torch.nn.Module):
             dict(params=self.mlp.l1.parameters()),
             dict(params=self.mlp.l2.parameters()),
             dict(params=self.mlp.l3.parameters()),
-            dict(params=self.sch.sch_layer.parameters()),
        ]

linking/training/trainer_qed.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import math
 from typing import Dict
 import torch
+from torch.distributions import Normal
 from tqdm import tqdm
 from torch_geometric.data import Data
 from linking.config.config import Config
@@ -59,10 +60,10 @@ class QEDTrainer:
             x_ligand = self.X_ligand_train[i]
             x_pocket = self.X_pocket_train[i]
 
-            prediction, mu, log_var = self.model(x_pocket, x_ligand)
-            log_var = torch.clip(log_var, -10, 10)
-            KL = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), 0)
-            loss_f = torch.nn.L1Loss()
+            prediction, mu, log_var, q = self.model(x_pocket, x_ligand)
+            KL = torch.distributions.kl_divergence(Normal(torch.zeros_like(mu), torch.ones_like(log_var)), q).mean()
+            #KL = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), 0)
+            loss_f = torch.nn.MSELoss()
             loss_normal = loss_f(prediction, self.Y_qed[x_ligand.name])
             loss = loss_normal + KL
             loss.backward()